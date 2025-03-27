73b340268c648ae453595660a8b2e6f9b23b27f2
Declan Nnadozie <zedonbiz@gmail.com>
2025-03-27 03:07:03 +0100

    update

--- lib/screens/QRcodeScreen.dart
+++ lib/screens/QRcodeScreen.dart
@@ -8,7 +8,6 @@
  * (at your option) any later version.
  */
 
-
 import 'dart:developer';
 import 'dart:io';
 
@@ -16,6 +15,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:qr_code_scanner_plus/qr_code_scanner_plus.dart';
+
 class QRcodeScreen extends StatefulWidget {
   const QRcodeScreen({Key? key}) : super(key: key);
 
@@ -42,96 +42,95 @@ class _QRcodeScreenState extends State<QRcodeScreen> {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
-      return Expanded(child: _buildQrView(constraints));
+      return Scaffold(
+        body: Column(
+          children: <Widget>[
+            Expanded(child: _buildQrView(constraints)),
+            // Expanded(
+            //   flex: 1,
+            //   child: FittedBox(
+            //     fit: BoxFit.contain,
+            //     child: Column(
+            //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            //       children: <Widget>[
+            //         if (result != null)
+            //           Text(
+            //               'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
+            //         else
+            //           const Text('Scan a code'),
+            //         Row(
+            //           mainAxisAlignment: MainAxisAlignment.center,
+            //           crossAxisAlignment: CrossAxisAlignment.center,
+            //           children: <Widget>[
+            //             Container(
+            //               margin: const EdgeInsets.all(8),
+            //               child: ElevatedButton(
+            //                   onPressed: () async {
+            //                     await controller?.toggleFlash();
+            //                     setState(() {});
+            //                   },
+            //                   child: FutureBuilder(
+            //                     future: controller?.getFlashStatus(),
+            //                     builder: (context, snapshot) {
+            //                       return Text('Flash: ${snapshot.data}');
+            //                     },
+            //                   )),
+            //             ),
+            //             Container(
+            //               margin: const EdgeInsets.all(8),
+            //               child: ElevatedButton(
+            //                   onPressed: () async {
+            //                     await controller?.flipCamera();
+            //                     setState(() {});
+            //                   },
+            //                   child: FutureBuilder(
+            //                     future: controller?.getCameraInfo(),
+            //                     builder: (context, snapshot) {
+            //                       if (snapshot.data != null) {
+            //                         return Text(
+            //                             'Camera facing ${describeEnum(snapshot.data!)}');
+            //                       } else {
+            //                         return const Text('loading');
+            //                       }
+            //                     },
+            //                   )),
+            //             )
+            //           ],
+            //         ),
+            //         Row(
+            //           mainAxisAlignment: MainAxisAlignment.center,
+            //           crossAxisAlignment: CrossAxisAlignment.center,
+            //           children: <Widget>[
+            //             Container(
+            //               margin: const EdgeInsets.all(8),
+            //               child: ElevatedButton(
+            //                 onPressed: () async {
+            //                   await controller?.pauseCamera();
+            //                 },
+            //                 child: const Text('pause',
+            //                     style: TextStyle(fontSize: 20)),
+            //               ),
+            //             ),
+            //             Container(
+            //               margin: const EdgeInsets.all(8),
+            //               child: ElevatedButton(
+            //                 onPressed: () async {
+            //                   await controller?.resumeCamera();
+            //                 },
+            //                 child: const Text('resume',
+            //                     style: TextStyle(fontSize: 20)),
+            //               ),
+            //             )
+            //           ],
+            //         ),
+            //       ],
+            //     ),
+            //   ),
+            // )
+          ],
+        ),
+      );
     });
-    // return Scaffold(
-    //   body: Column(
-    //     children: <Widget>[
-    //       Expanded( child: _buildQrView(context)),
-    //       // Expanded(
-    //       //   flex: 1,
-    //       //   child: FittedBox(
-    //       //     fit: BoxFit.contain,
-    //       //     child: Column(
-    //       //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    //       //       children: <Widget>[
-    //       //         if (result != null)
-    //       //           Text(
-    //       //               'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
-    //       //         else
-    //       //           const Text('Scan a code'),
-    //       //         Row(
-    //       //           mainAxisAlignment: MainAxisAlignment.center,
-    //       //           crossAxisAlignment: CrossAxisAlignment.center,
-    //       //           children: <Widget>[
-    //       //             Container(
-    //       //               margin: const EdgeInsets.all(8),
-    //       //               child: ElevatedButton(
-    //       //                   onPressed: () async {
-    //       //                     await controller?.toggleFlash();
-    //       //                     setState(() {});
-    //       //                   },
-    //       //                   child: FutureBuilder(
-    //       //                     future: controller?.getFlashStatus(),
-    //       //                     builder: (context, snapshot) {
-    //       //                       return Text('Flash: ${snapshot.data}');
-    //       //                     },
-    //       //                   )),
-    //       //             ),
-    //       //             Container(
-    //       //               margin: const EdgeInsets.all(8),
-    //       //               child: ElevatedButton(
-    //       //                   onPressed: () async {
-    //       //                     await controller?.flipCamera();
-    //       //                     setState(() {});
-    //       //                   },
-    //       //                   child: FutureBuilder(
-    //       //                     future: controller?.getCameraInfo(),
-    //       //                     builder: (context, snapshot) {
-    //       //                       if (snapshot.data != null) {
-    //       //                         return Text(
-    //       //                             'Camera facing ${describeEnum(snapshot.data!)}');
-    //       //                       } else {
-    //       //                         return const Text('loading');
-    //       //                       }
-    //       //                     },
-    //       //                   )),
-    //       //             )
-    //       //           ],
-    //       //         ),
-    //       //         Row(
-    //       //           mainAxisAlignment: MainAxisAlignment.center,
-    //       //           crossAxisAlignment: CrossAxisAlignment.center,
-    //       //           children: <Widget>[
-    //       //             Container(
-    //       //               margin: const EdgeInsets.all(8),
-    //       //               child: ElevatedButton(
-    //       //                 onPressed: () async {
-    //       //                   await controller?.pauseCamera();
-    //       //                 },
-    //       //                 child: const Text('pause',
-    //       //                     style: TextStyle(fontSize: 20)),
-    //       //               ),
-    //       //             ),
-    //       //             Container(
-    //       //               margin: const EdgeInsets.all(8),
-    //       //               child: ElevatedButton(
-    //       //                 onPressed: () async {
-    //       //                   await controller?.resumeCamera();
-    //       //                 },
-    //       //                 child: const Text('resume',
-    //       //                     style: TextStyle(fontSize: 20)),
-    //       //               ),
-    //       //             )
-    //       //           ],
-    //       //         ),
-    //       //       ],
-    //       //     ),
-    //       //   ),
-    //       // )
-    //     ],
-    //   ),
-    // );
   }
 
   Widget _buildQrView(BoxConstraints constraints) {
@@ -164,6 +163,7 @@ class _QRcodeScreenState extends State<QRcodeScreen> {
         });
       }
     });
+
   }
 
   void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
